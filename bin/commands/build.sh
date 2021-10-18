196f566d5f5f91a3b0c7db60d253e96fdf9c07f7
Tobias Umbach <umbach@men-at-work.de>
2021-10-18 14:19:59 +0200

    tweak run message

--- bin/commands/build.sh
+++ bin/commands/build.sh
@@ -1,10 +1,12 @@
 # just in case we're not in the devbox repo
 cd "$devbox_src_dir"
 
+image_tag=latest
+devbox_version="$image_tag"
+
 if [[ $# != 1 ]]; then
   echo_info "No version given; will tag with ':latest'"
   echo_info "To create a versioned release build, run this command with a version argument"
-  image_tag="latest"
 elif ! git tag --points-at HEAD | grep --quiet "^$1$"; then
   echo_error "Tried to build release for version $1, but no such tag points at HEAD."
   echo_error "To tag a release build, HEAD must have a matching version tag."
@@ -14,9 +16,18 @@ else
   image_tag="$1"
 fi
 
+if [[ "$devbox_version" == latest ]]; then
+  devbox_version="$(git rev-parse --short HEAD)"
+
+  if [[ $(git diff --stat) != "" ]]; then
+    devbox_version="$devbox_version*"
+  fi
+fi
+
 docker_args=(
   build
 
+  --build-arg DEVBOX_VERSION="$devbox_version"
   --tag "$devbox_image:$image_tag"
 
   ./docker

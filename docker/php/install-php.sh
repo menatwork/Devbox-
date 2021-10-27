a1475f634538c85f0b24fd4eae8abca445fc69a4
Tobias Umbach <umbach@men-at-work.de>
2021-10-27 14:19:41 +0200

    restructure php install script

--- docker/php/install-php.sh
+++ docker/php/install-php.sh
@@ -1,3 +1,15 @@
+# see https://launchpad.net/~ondrej/+archive/ubuntu/php
+php_versions=(
+  5.6
+  7.3
+  7.4
+  8.0
+)
+
+# see https://getcomposer.org/download/
+composer_1_version=1.10.23
+composer_2_version=2.1.9
+
 install_php_versions() {
   local p=()
 
@@ -29,19 +41,14 @@ install_php_versions() {
 build-helper install software-properties-common gnupg curl unzip
 apt-add-repository --yes ppa:ondrej/php > /dev/null
 
-composer_1_version=1.10.23
-composer_2_version=2.1.9
-
-install_php_versions 5.6 7.3 7.4 8.0
+install_php_versions "${php_versions[@]}"
 
 # the default binary should never be called, that's what the php shim inside the
 # container is for
 rm /usr/bin/php
 
 curl --silent --location \
-    https://getcomposer.org/download/"$composer_1_version"/composer.phar \
-    --output /usr/local/bin/composer1 \
-    https://getcomposer.org/download/"$composer_2_version"/composer.phar \
-    --output /usr/local/bin/composer
+  "https://getcomposer.org/download/$composer_1_version/composer.phar" --output /usr/local/bin/composer1 \
+  "https://getcomposer.org/download/$composer_2_version/composer.phar" --output /usr/local/bin/composer
 
 chmod +x /usr/local/bin/composer1 /usr/local/bin/composer

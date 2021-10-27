878db0ca45abc3969318d9e7e687bcdbf3b20469
Tobias Umbach <umbach@men-at-work.de>
2021-10-27 16:28:07 +0200

    run all services as devbox user

--- docker/apache2/install-apache2.sh
+++ docker/apache2/install-apache2.sh
@@ -1,12 +1,10 @@
-build-helper install apache2
+build-helper install apache2 libcap2-bin
 
 a2enmod --quiet proxy_fcgi proxy_http proxy_wstunnel setenvif rewrite headers
 a2enconf --quiet devbox
 a2dissite --quiet 000-default
 
-cat >> /etc/apache2/envvars <<EOF
+# allow apache to bind to port 80 without being root
+setcap CAP_NET_BIND_SERVICE=+eip /usr/sbin/apache2
 
-# devbox modifications
-export APACHE_RUN_USER=devbox
-export APACHE_RUN_GROUP=devbox
-EOF
+echo APACHE_RUN_USER=devbox >> /etc/apache2/envvars

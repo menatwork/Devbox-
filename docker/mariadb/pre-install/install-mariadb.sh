ab2dfb72b980709e843f44ad3c0f1c7b3a6d3ed7
Tobias Umbach <umbach@men-at-work.de>
2021-11-23 21:23:15 +0100

    mute priv drop warning from mariadb

--- docker/mariadb/pre-install/install-mariadb.sh
+++ docker/mariadb/pre-install/install-mariadb.sh
@@ -1,1 +1,5 @@
 build-helper install mariadb-server
+
+# mutes warning about user option not being available because we already run
+# mariadb as an unprivileged user
+sed -iE 's/^user\s\+=\s\+mysql$/#\0/' /etc/mysql/mariadb.conf.d/50-server.cnf
